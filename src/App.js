f386d3685fe74b9a123b856a991c454afc9f8fc3
Devender kumar <devender.kumar@incaendo.com>
2022-02-09 17:48:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from './layout';
+function App() {
+  return (
+    <div className="App">
+     <Heading/>
+    </div>
+  );
+}
+
+export default App;
